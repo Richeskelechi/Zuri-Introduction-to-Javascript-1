b31524d68be604305d96a67fd413ec2f27f0d22b
Riches <richeskelechi94.rk@gmail.com>
2022-06-21 23:06:56 +0100

    Done with The Task

---
+++ index.js
@@ -0,0 +1,7 @@
+let yourName = prompt('Please What Is Your Name: ')
+let yourHeight = prompt('Please What Is Your Height: ')
+let yourCounty = prompt('Please What Is Your Country: ')
+
+document.querySelector('#name').innerHTML = yourName
+document.querySelector('#height').innerHTML = yourHeight
+document.querySelector('#country').innerHTML = yourCounty
